afe9b804b9dacd6e5282632d0bd26e1f9e9268e5
wengkhing <kheryee@live.com>
2015-09-03 15:35:16 +0800

    add login signout logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,48 @@ get '/' do
   @posts = Post.all
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+# add validations later
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  unless (user.nil?)
+    if (password == user.password)
+      session[:user] = user.username
+      redirect '/'
+    else
+      @error = "password does not match."
+    end
+  else
+    @error = "user does not exist."
+  end
+  redirect "/login?error=#{@error}"
+end
+
+post '/signup' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if (user.nil?)
+    User.create(username: username, password: password)
+    redirect '/'
+  else
+    @error = "user already exist."
+  end
+  redirect "/login?error=#{@error}"
+end
+
+get '/signout' do
+  session[:user] = nil
+
+  redirect '/'
+end
