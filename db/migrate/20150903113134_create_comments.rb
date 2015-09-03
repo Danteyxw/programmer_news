95066780b95528536baf25bcc9f6b4ea5b5a9723
wengkhing <kheryee@live.com>
2015-09-03 12:19:27 +0800

    implement index page and add post_id to comments table

--- db/migrate/20150903113134_create_comments.rb
+++ db/migrate/20150903113134_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.integer :user_id, null: false
+      t.integer :post_id, null: false
       t.string :content, null: false
       t.timestamp null: false
     end
