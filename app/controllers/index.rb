dbba67050a583c78bea48cc8d0a11c2669d7b66f
wengkhing <kheryee@live.com>
2015-09-03 17:28:36 +0800

    add user profile, user posts and user comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -29,7 +29,7 @@ end
 
 get '/submit' do
   unless (session[:user].nil?)
-    erb :submissions
+    erb :submit
   else
     @error = "Please login or sign up to submit a post."
     redirect "/login?error=#{@error}"
@@ -83,4 +83,40 @@ post '/post/:id/comment' do
   @user = User.find_by(username: session[:user])
   @post.comments.create(content: params[:content], user_id: @user.id)
   redirect "/post/#{params[:id]}"
+end
+
+get '/profile/:id' do
+  @user = User.find(params[:id])
+
+  unless (@user.nil?)
+    erb :profile
+  else
+    @error = "Please login or sign up to view your profile."
+    redirect "/login?error=#{@error}"
+  end
+end
+
+get '/profile/posts/:id' do
+  @user = User.find(params[:id])
+  @posts = @user.posts
+
+  erb :posts
+end
+
+get '/profile/comments/:id' do
+  @user = User.find(params[:id])
+  @comments = @user.comments
+
+  erb :comments
+end
+
+post '/post/:id/delete' do
+  post = Post.find(params[:id])
+  if (session[:user] == post.user.username)
+    post.delete
+    redirect "/profile/#{post.user.id}/posts"
+  else
+    @error = "You are not #{post.user.username}."
+    redirect "/profile/#{post.user.id}/posts/?error=#{@error}"
+  end
 end
