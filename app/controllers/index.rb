80bc719b0e982aacb75dabe1f75cb78772ab95cb
wengkhing <kheryee@live.com>
2015-09-03 16:39:57 +0800

    implement comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -77,3 +77,10 @@ get '/signout' do
 
   redirect '/'
 end
+
+post '/post/:id/comment' do
+  @post = Post.find(params[:id])
+  @user = User.find_by(username: session[:user])
+  @post.comments.create(content: params[:content], user_id: @user.id)
+  redirect "/post/#{params[:id]}"
+end
